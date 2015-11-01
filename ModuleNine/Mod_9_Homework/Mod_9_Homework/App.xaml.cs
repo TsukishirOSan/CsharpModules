16db607ffb5ef660d4c115f7c00c7fb42ab2b2b4
TsukishirOSan <faiza.harbi@gmail.com>
2015-11-01 10:59:59 +0100

    C# Modules
    DEV204x Programming with C#
    From the most basic programs to the use of LINQ, Tasks xaml etc....

--- ModuleNine/Mod_9_Homework/Mod_9_Homework/App.xaml.cs
+++ ModuleNine/Mod_9_Homework/Mod_9_Homework/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace StudentList
+namespace Mod_9_Homework
 {
     /// <summary>
     /// Interaction logic for App.xaml
