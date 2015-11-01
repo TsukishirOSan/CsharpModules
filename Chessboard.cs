1e51031fc7299b04b58dd89da7a13d9f03f6610b
TsukishirOSan <faiza.harbi@gmail.com>
2015-11-01 10:50:09 +0100

    C# Modules
    So far 11 assignment in C# , from the most basic programming to xaml,
    LINQ etc.... edX DEV204x: Programming with C#

---
+++ Chessboard.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Assignmt2
+{
+    class Chessboard
+    {
+        static void Main(String[] args)
+        {
+            /**
+             * For this assignment, you will create the pattern of a chess board that is 8 x 8.  Use X and O to represent the squares.
+
+                Create the appropriate nested looping structure to output the characters in an 8 x 8 grid on the screen using Console.Write() or Console.WriteLine() as appropriate.
+                Include a decision structure to ensure that alternate rows start with opposite characters as a real chess board alternates the colors among rows.
+                This is what your output should look like.
+
+                XOXOXOXO
+                OXOXOXOX
+                XOXOXOXO
+                OXOXOXOX
+                XOXOXOXO
+                OXOXOXOX
+                XOXOXOXO
+                OXOXOXOX
+
+                Grading Criteria:
+
+                Used a nested loop
+                Used a decision structure to flip row output
+                Output is correct per above image
+             * */
+            
+            int BoardDimention = 8;
+            
+            // implement the algorithm to display an 8x8 chessboard
+
+            // this loop deals with rows one by one
+            for(int i = 0; i < BoardDimention; i++)
+            {
+                // this loop deals with each character of the chessboard
+                for(int j = 0; j < BoardDimention; j++)
+                {
+                    if(i % 2 == 0)
+                    {
+                        if(j % 2 != 0)
+                        {
+                            Console.Write('X');
+                        }
+                        else
+                        {
+                            Console.Write('O');
+                        }
+                    }
+                    else
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Console.Write('O');
+                        }
+                        else
+                        {
+                            Console.Write('X');
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,44 @@
+# CsharpModules
+Module 1
+/* create variable and assign them a value
+* So that those values can be changed
+* later for the assignment's purpose 
+* */
+
+Module 2
+/**
+* For this assignment, you will create the pattern of a chess board that is 8 x 8. Use X and O to represent the squares.
+
+Create the appropriate nested looping structure to output the characters in an 8 x 8 grid on the screen using Console.Write() or Console.WriteLine() as appropriate.
+Include a decision structure to ensure that alternate rows start with opposite characters as a real chess board alternates the colors among rows.
+This is what your output should look like.
+
+XOXOXOXO
+OXOXOXOX
+XOXOXOXO
+OXOXOXOX
+XOXOXOXO
+OXOXOXOX
+XOXOXOXO
+OXOXOXOX
+
+Grading Criteria:
+
+Used a nested loop
+Used a decision structure to flip row output
+Output is correct per above image
+* */
+
+
+
+
+Module 3:
+ /**
+    -Using the above partial code sample, complete the method for getting student data.
+    -Create a method to get information for a teacher, a course, a program, and a degree using a similar method as above.
+    -Create methods to print the information to the screen for each object such as static void PrintStudentDetails(...).
+    -From within Main(), call each of the methods to prompt for input from a user of your application.
+    -Just enter enough information to show you understand how to use methods.  (At least three attributes each).
+    -Assign the values that are input, to the proper variables.
+    or DateTime to determine ways to check if a valid date is entered
+     */
